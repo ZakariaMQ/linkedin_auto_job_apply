e9e7f0b6b33ba32cc01d3ec2542e33a6bc5af7d9
zakariaMQ <zm554765@gmail.com>
2024-05-18 10:02:58 +0100

    minor improvements

--- background.js
+++ background.js
@@ -1,27 +1,21 @@
 browser.runtime.onInstalled.addListener(() => {
-    alert("LinkedIn auto Job Apply add-on installed");
-  });
-  
-browser.browserAction.onClicked.addListener((tab) => {
-    browser.tabs.executeScript(tab.id, {
-      file: 'content.js'
-    });
-  });
-  
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "startApplying") {
-      browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        browser.tabs.sendMessage(tabs[0].id, {
-          action: "startApplying",
-          numJobs: request.numJobs,
-          delay: request.delay
-        });
-      });
-    }
+  alert("LinkedIn auto Job Apply add-on installed successfully!");
 });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "displayMessage") {
-       chrome.runtime.sendMessage({ action: "displayMessage", message: message.message });
-    }
+browser.browserAction.onClicked.addListener((tab) => {
+  browser.tabs.executeScript(tab.id, {
+    file: 'content.js'
+  });
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "startApplying") {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, {
+        action: "startApplying",
+        numJobs: request.numJobs,
+        delay: request.delay
+      });
+    });
+  }
 });
