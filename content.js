b8cc494752a00956453092fd7f1729db19f86273
zakariaMQ <zm554765@gmail.com>
2024-05-21 21:12:00 +0100

    updated 'content.js'

--- content.js
+++ content.js
@@ -9,6 +9,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+function sleep(t) {
+  const seconds = t * 1000
+  setTimeout(() => {
+      console.info(`Waited for ${seconds} second!`);
+    }, seconds);
+}
+
 function scrollAndFindJobs(numJobs, delay) {
   const jobsListContainer = document.querySelector('.jobs-search-results-list');
 
@@ -17,31 +24,61 @@ function scrollAndFindJobs(numJobs, delay) {
       return;
   }
 
-  let lastHeight = jobsListContainer.scrollHeight;
-  let jobElements = [];
+
+  let currentJobIndex = 0;
 
   const intervalId = setInterval(() => {
-      jobsListContainer.scrollTop = jobsListContainer.scrollHeight;
+      const jobElements = jobsListContainer.querySelectorAll('.display-flex.job-card-container.relative');
 
-      setTimeout(() => {
-          const newJobElements = jobsListContainer.querySelectorAll('.display-flex.job-card-container.relative');
-          jobElements = Array.from(newJobElements);
-          console.log(`Found ${jobElements.length}`);
+      if (currentJobIndex >= jobElements.length) {
+          console.log('No more jobs to apply.');
+          clearInterval(intervalId);
+          return;
+      }
 
-          const newHeight = jobsListContainer.scrollHeight;
+      const currentJobElement = jobElements[currentJobIndex];
+      currentJobElement.scrollIntoView();
+      currentJobElement.click()
+      const easyApplyButton = document.getElementsByClassName("jobs-apply-button artdeco-button artdeco-button--3 artdeco-button--primary ember-view")[0];
 
-          if (newHeight === lastHeight || jobElements.length >= numJobs) {
-              clearInterval(intervalId);
-              console.log('Finished scrolling and loading jobs.');
+      if (easyApplyButton) {
+          easyApplyButton.click();
+          console.log(`Clicked Easy Apply for job ${currentJobIndex + 1}`);
+          const res = applyForJobs(numJobs, delay)
+          console.log(res)
+      } else {
+          console.log(`No Easy Apply button found for job ${currentJobIndex + 1}`);
+      }
 
-          } else {
-              lastHeight = newHeight;
-          }
-      }, 200);
+      currentJobIndex++;
+
+      if (currentJobIndex >= numJobs) {
+          console.log('Reached the number of jobs to apply.');
+          clearInterval(intervalId);
+      }
+
   }, 1000);
-
 }
 
-function applyForJobs(numJobs, delay, jobElements) {
 
+function applyForJobs() {
+  try {
+      const box = document.querySelector(".jobs-easy-apply-content");
+      let next = box.querySelector("span.artdeco-button__text");
+      for (let i = 0; i<=5; i++) {
+          next.scrollIntoView()
+          next.click();
+          sleep(1)
+          if (!document.querySelector(".jobs-easy-apply-content")) {
+              console.log("Applied successfully!")
+              return true;
+          }
+          next = box.querySelector("span.artdeco-button__text");
+          sleep(1)
+      };
+      return true;
+
+  } catch {
+      return false;
+  }
 }
